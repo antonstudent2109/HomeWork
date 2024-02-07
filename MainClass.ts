46be14438bb23fddeecb6a4d736c9d3771f57f9f
antonstudent2109 <159119634+antonstudent2109@users.noreply.github.com>
2024-02-07 13:59:03 +0100

    Fixes of homeowrk

--- MainClass.ts
+++ MainClass.ts
@@ -1,6 +1,19 @@
-class MainClass
+class MainClass() {
+    cy.visit.mainPage();
+    cy.userNameField.type("standard_user");
+    cy.passwordField.type("secret_saucetest");
+    cy.loginButton.click();
+    cy.errorMessage.should('be.true');//negative result
+
+    cy.userNameField.type("standard_user");
+    cy.passwordField.type("secret_sauce");//positive result
+    cy.loginSuccess.should('be.true') //postitive result
+
+
+    cy.cartButton.click(); //add to cart button is claickable
+
+}
 
 
 
-cy.visit('https://example.cypress.io')
 
--- Selectors.ts
+++ Selectors.ts
@@ -3,9 +3,26 @@ class selectors {
         return cy.get('https://www.saucedemo.com');
     }
 
+    static get userNameField(){
+        return cy.get([id="user-name"]);
+    }
+
+    static get passwordField(){
+        return cy.get([id="password"]);
+    }
+
+    static get loginButton(){
+        return cy.get([id="login-button"]);
+    }
+
+    static get errorMessage(){
+        return cy.get([class="error-button"]);
+    }
+
     static get loginSuccess() {
-        return cy.get('id='inventory_container');
+        return cy.get(id="inventory_container");
     };
 
     static get cartButton() {
-        return cy.get('id='add-to-cart-test.allthethings()-t-shirt-(red)');
+        return cy.get(id="add-to-cart-test.allthethings()-t-shirt-(red)");
+    };
